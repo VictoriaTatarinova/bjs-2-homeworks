48ad1b2a4d234b959ae8a6fed9c480402f886b6a
VictoriaTatarinova <chiarouomo@gmail.com>
2022-10-25 19:54:49 +0200

    2.functions initial commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -2,30 +2,44 @@
 function getArrayParams(arr) {
   let min, max, sum, avg;
 
-  // Ваш код
+  min = Infinity;
+    max = - Infinity;
+    sum = 0;
+
+    for (let number of arr) {
+      if (number < min) {
+        min = number
+      }
+      if (number > max) {
+        max = number;
+      }
+      sum += number;
+    }
+    avg = parseFloat((sum / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
+    for (let number of arr) {
+      sum += number;
+    }
+    return sum;
+  }
 
-  // Ваш код
-
-  return sum;
-}
-
-function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
-  return max;
+  function makeWork(arrOfArr, func) {
+    let max = - Infinity;
+    for (let arr of arrOfArr) {
+      if (func(arr) > max) {
+        max = func(arr)
+      }
+    }
+    return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  return getArrayParams(arr).max - getArrayParams(arr).min;
 }
