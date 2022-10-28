970424abf4ed2c5c297cf7e67489080e7e001088
VictoriaTatarinova <chiarouomo@gmail.com>
2022-10-28 14:18:39 +0200

    4.objects Initial commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,45 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
+let student1 = new Student("Alex", "male", 37);
+let student2 = new Student("Kate", "female", 29);
+let student3 = new Student("Richard", "male", 21);
+
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+    this.subject = subjectName;
+}
+
+Student.prototype.addMark = function(mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function(...marks) {
+  if (this.marks === undefined) {
+      this.marks = [...marks];
+  } else {
+      this.marks.push(...marks);
+    }
+  }
+
+Student.prototype.getAverage = function() {
+  let sum = 0;
+  for (i = 0; i < this.marks.length; i++) {
+    sum += this.marks[i];
+  }
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
 
 // ваш код для остальных методов
