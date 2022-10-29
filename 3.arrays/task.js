7ec9cc3f9fc637657c8a82135272b0e2d9828738
VictoriaTatarinova <chiarouomo@gmail.com>
2022-10-29 19:40:04 +0200

    3.arrays Fixed Task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,17 +1,17 @@
 function compareArrays(arr1, arr2) {
-  let result = true;
-  if (arr1.length != arr2.length) {
-    result = false;
+  let result;
+  if (arr1.length > arr2.length || arr1.length < arr2.length) {
+    return false;
+  } else {
+    result = arr1.every ((currentValuefromArr1, index) => currentValuefromArr1 === arr2[index]);
+    return result;
   }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] != arr2[i]) {
-      result = false;
-    }
-  }
-  return result;
 }
 
 function advancedFilter(arr) {
-  let resultArr = arr.filter(item => item > 0).filter(item => item % 3 == 0).map(item => item *= 10);
+  let resultArr= arr
+  .filter((item) => item > 0)
+  .filter ((item) => item % 3 === 0)
+  .map ((item) => item * 10);
   return resultArr;
 }
